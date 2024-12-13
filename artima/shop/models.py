ee6f9e21749f04e0dc29b36caed0aba39ac7055f
foxx_ill <ilyalebel@gmail.com>
2024-12-13 22:35:05 +0300

    модель категории создана

--- artima/shop/models.py
+++ artima/shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """модель для категорий продуктов"""
+    name = models.CharField(max_length = 200, db_index = True)
+    # поле под ссылку на категорию
+    slug = models.SlugField(max_length = 200, unique = True)
+    image = models.ImageField(upload_to='images/category', default = 'image/default.jpg' , blank = True)
+    description = models.TextField(blank = True)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
+    def __str__(self):
+        return self.name
+    
+    def tags(self):
+        tags = []
+        for word in self.description.split(' '):
+            if word[0] == '#':
+                tags.append(word)
+        return tags
