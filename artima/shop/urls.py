48e64daf12ab02f88e32aa4d1708c83c1019796f
foxx_ill <ilyalebel@gmail.com>
2024-12-14 16:23:43 +0300

    сделал вывод страницы профиля

--- artima/shop/urls.py
+++ artima/shop/urls.py
@@ -8,7 +8,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.home, name='home'),
     # path('category&<str:category_slug>/', views.category, name = 'category'),
-    # path('profile/', views.profile, name = 'profile'),
+    path('profile/', views.profile, name = 'profile'),
     # path('register/', views.register, name = 'register'),
     # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='registration/login.html'), name='logout'),
